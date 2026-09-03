790aab9484e85d24f10303e643c38a95417f5b9f
Edward West <edwardwestdev@gmail.com>
2026-09-03 16:34:13 -0700

    added 8N1 impl

--- nucleo-bitbang/src/uart.rs
+++ nucleo-bitbang/src/uart.rs
@@ -16,9 +16,10 @@ impl<'a> BitBangUart<'a> {
             bit_duration,
         }
     }
-    pub async fn start_transfer(&mut self, send_buf: &[u8], message: &[u8]) {
+    pub async fn start_transfer_8n1(&mut self, send_buf: &[u8], message: &[u8]) {
         for &byte in message {
-            self.tx.set_low(); // start 
+            self.tx.set_high(); //start bit
+            self.tx.set_low(); // start
             Timer::after(self.bit_duration).await;
 
             for i in 0..8 {
@@ -30,13 +31,14 @@ impl<'a> BitBangUart<'a> {
                 Timer::after(self.bit_duration).await;
             }
         }
-        self.tx.set_high();
+        self.tx.set_high(); // stop bit 
         Timer::after(self.bit_duration).await;
+        self.tx.set_low(); // reset for next byte
     }
 }
 pub async fn uart_bitbang(tx: Output<'_>, rx: Input<'_>) {
     let mut uart = BitBangUart::new(tx, rx, 9600);
 
     let msg: &[u8] = b"Hello, Uart!";
-    uart.start_transfer(&[0xf9], msg).await;
+    uart.start_transfer_8n1(&[0xf9], msg).await;
 }
