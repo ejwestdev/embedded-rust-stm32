0c172e20a317b7ddc3c80c0ad06724ccd0f58ca6
Edward West <edwardwestdev@gmail.com>
2026-09-11 16:53:18 -0700

    i2c implemented

---
+++ nucleo-bitbang/src/i2c.rs
@@ -0,0 +1,61 @@
+use embassy_stm32::gpio::OutputOpenDrain;
+use embassy_time::{Duration, Timer};
+pub struct BitBangI2C<'a> {
+    sda: OutputOpenDrain<'a>,
+    scl: OutputOpenDrain<'a>,
+    delay: Duration,
+}
+impl<'a> BitBangI2C<'a> {
+    pub fn new(sda: OutputOpenDrain<'a>, scl: OutputOpenDrain<'a>, half_period_ns: u32) -> Self {
+        let delay = Duration::from_nanos(half_period_ns as u64);
+        Self { sda, scl, delay }
+    }
+    pub async fn i2c_transfer(&mut self, data: u8) -> u8 {
+        for i in 0..8 {
+            let bit = (data >> (7 - i)) & 1 == 1;
+            if bit {
+                self.sda.set_high();
+            } else {
+                self.sda.set_low();
+            }
+            Timer::after(self.delay).await;
+            self.scl.set_high(); // slave samples the bit here
+            Timer::after(self.delay).await;
+            self.scl.set_low();
+        }
+
+        self.sda.set_high();
+        Timer::after(self.delay).await;
+        self.scl.set_high();
+        Timer::after(self.delay).await;
+        let acked = self.sda.is_low();
+        self.scl.set_low();
+
+        if acked { 0 } else { 1 }
+    }
+    pub async fn start_transfer(&mut self, recv_buf: &mut [u8], data_buf: &[u8]) {
+        self.sda.set_low();
+        Timer::after(self.delay).await;
+        self.scl.set_low();
+        Timer::after(self.delay).await;
+
+        for (i, &byte) in data_buf.iter().enumerate() {
+            recv_buf[i] = self.i2c_transfer(byte).await;
+        }
+        defmt::info!("i2c ack bits: {:?}", recv_buf);
+
+        self.sda.set_low();
+        Timer::after(self.delay).await;
+        self.scl.set_high();
+        Timer::after(self.delay).await;
+        self.sda.set_high();
+        Timer::after(self.delay).await;
+    }
+}
+
+pub async fn i2c_bitbang(sda: OutputOpenDrain<'_>, scl: OutputOpenDrain<'_>) {
+    let mut i2c = BitBangI2C::new(sda, scl, 5_000);
+    let data = [92, 134, 110, 97, 196, 95, 78, 243];
+    let mut recv_buf = [0, 0, 0, 0, 0, 0, 0, 0];
+    i2c.start_transfer(&mut recv_buf, &data).await;
+}
--- nucleo-bitbang/src/main.rs
+++ nucleo-bitbang/src/main.rs
@@ -1,12 +1,14 @@
 #![no_std]
 #![no_main]
+mod i2c;
 mod panic;
 mod spi;
 mod uart;
+use crate::i2c::i2c_bitbang;
 use crate::spi::spi_bitbang;
 use crate::uart::uart_bitbang;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
+use embassy_stm32::gpio::{Input, Level, Output, OutputOpenDrain, Pull, Speed};
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
@@ -19,7 +21,10 @@ async fn main(_spawner: Spawner) {
     let pico = Output::new(p.PA7, Level::Low, Speed::High);
     let poci = Input::new(p.PA6, Pull::None);
     let cs = Output::new(p.PA4, Level::High, Speed::High);
-
+    //i2c
+    let sda = OutputOpenDrain::new(p.PC0, Level::High, Speed::High);
+    let scl = OutputOpenDrain::new(p.PC1, Level::High, Speed::High);
     spi_bitbang(sck, pico, poci, cs).await;
     uart_bitbang(tx, rx).await;
+    i2c_bitbang(sda, scl).await;
 }
